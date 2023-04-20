f117d5555bb7ef99745ecf6707ab4b5ca22aa688
Carlos Azocar Riquelme <carlos.azocar.riquelme@gmail.com>
2023-04-20 18:52:52 -0400

    First commit

---
+++ codigos_modulo_3_python/codigos_sesion_2/hola.py
@@ -0,0 +1,13 @@
+print("hola")
+
+edad = 50
+sueldo = 1000.50
+nombre = "Carlos"
+isTeacher = False
+
+print(edad, sueldo, nombre, isTeacher)
+print(edad, sueldo, nombre, isTeacher)
+numero_1 = 1
+numero_2 = 3
+
+print(numero_1+numero_2)
